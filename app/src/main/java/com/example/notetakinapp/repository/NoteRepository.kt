3eb4e4f7bcce9e08847fc96f3a57cb81701717a5
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-05-03 16:10:44 +0300

    Created NoteRepository (multithread fun + normal fun)

---
+++ app/src/main/java/com/example/notetakinapp/repository/NoteRepository.kt
@@ -0,0 +1,17 @@
+package com.example.notetakinapp.repository
+
+import com.example.notetakinapp.database.NoteDatabase
+import com.example.notetakinapp.model.Note
+
+class NoteRepository(
+    private val db: NoteDatabase
+    ) {
+
+    private val noteDB =  db.getNoteDao()
+    suspend fun insertNote(note: Note) = noteDB.insertNote(note)
+    suspend fun deleteNote(note: Note) = noteDB.deleteNote(note)
+    suspend fun updateNote(note: Note) = noteDB.updateNote(note)
+
+    fun getAllNotes() = noteDB.getAllNotes()
+    fun seachNote(query: String) = noteDB.searchNote(query)
+}
